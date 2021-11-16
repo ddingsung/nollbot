414ed4708fc8546a652b4d73b244a7aef46e300d
ddingsung <91331587+ddingsung@users.noreply.github.com>
2021-11-16 21:24:24 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,7 +129,7 @@ async def 정보(ctx):
 @bot.command(name="일정투표")
 async def reaction_to_message(ctx):
     embed=discord.Embed(title= "이번주 길드일정표", description ="참여하고싶은 시간대 번호를 눌러서 참가유무를 알려주세요 !",color=0x6892f3)
-    embed.add_field(name="수요일", value="1️⃣ 아르고스 20:30           2️⃣ [노말]발탄 21:30", inline=False)
+    embed.add_field(name="수요일", value="1️⃣ [노말]발탄 20:30           2️⃣ [노말]아브렐슈드(1,2관) 21:30", inline=False)
     embed.add_field(name="목요일", value="3️⃣ [하드]발탄 20:30         4️⃣ [노말]비아키스 22:30", inline=False)
     embed.add_field(name="금요일", value="5️⃣ [하드]비아키스 20:30    6️⃣ [노말]발탄 22:30", inline=False)
     embed.add_field(name="토요일", value="7️⃣ [하드]발탄 17:30         8️⃣ [데자뷰]아브렐슈드 19:30", inline=False)
